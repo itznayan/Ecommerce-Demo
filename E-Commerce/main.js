e67d2680b606c6b778f4321dfc2ffc31fda328ac
Mahera Nayan <136584376+itznayan@users.noreply.github.com>
2024-04-04 00:38:18 +0530

    Done

---
+++ E-Commerce/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import products from "./api/products.json";
+
+import { showProductContainer } from "./homeProductCards";
+// display all products
+
+showProductContainer(products);
